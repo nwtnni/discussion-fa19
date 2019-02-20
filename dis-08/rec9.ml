436eb8c5e86fdd6bfba3a55145f957470117e3d1
Kenneth Fang <kenneth@LAPTOP-O7IMHKPC.localdomain>
2019-02-20 09:59:15 -0500

    rec9 template

---
+++ dis-08/rec9.ml
@@ -0,0 +1,4 @@
+module type BoundedQueue = sig
+(* TODO Fill me out! Think carefully about what a BoundedQueue might need 
+   In its signature, and we will share implementations with the class. *)
+end
