6d137c7b13f6ab01d957302ac88d177c720c71c6
Kenneth Fang <kenneth@LAPTOP-O7IMHKPC.localdomain>
2019-02-20 09:57:10 -0500

    Added dis8 exercise

---
+++ dis-08/spec_game.ml
@@ -0,0 +1,11 @@
+(** [sqr x] is the square root of [x]. *)
+val sqr : int -> int
+
+(** returns a list *)
+val reverse : 'a list -> 'a list
+
+(** sorts a list *)
+val sort: int list -> int list
+
+(** removes element at index from list *)
+val remove: int -> 'a list -> 'a list
