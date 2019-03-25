d63c3eed699a9146893c49096c6e37f71c559d7b
Kenneth Fang <kenneth@LAPTOP-O7IMHKPC.localdomain>
2019-03-25 12:18:41 -0400

    Fixed typos in monad.ml and slides, added monad_sol

--- dis-16/exercises/monad_sol.ml
+++ dis-16/exercises/monad_sol.ml
@@ -6,8 +6,11 @@ end
 
 module Optional = struct
     type 'a t = 'a option
-    let return a = failwith "Unimplemented"
-    let (>>=) a f = failwith "Unimplemented"
+    let return a = Some a
+    let (>>=) a f = 
+      match a with
+      | Some a -> f a
+      | None -> None
 end
 
 (** Try doing the following in UTOP: 
@@ -16,23 +19,32 @@ List.find_opt (fun x -> x = 5) [5;1] >>= (fun x -> return x)
 
 module Error = struct
     type 'a t = Error of string | Val of 'a
-    let return a = failwith "Unimplemented"
-    let (>>=) a f = failwith "Unimplemented"
+    let return a = Val a
+    let (>>=) a f = 
+      match a with
+      | Val a -> f a 
+      | Error s -> print_endline; Error s
 end
 
 module Lazy = struct
     type 'a t = unit -> 'a
-    let return a = failwith "Unimplemented"
-    let (>>=) a f = failwith "Unimplemented"
+    let return a = fun () -> a
+    let (>>=) a f = fun () -> f (a ()) ()
 
     (** An extra function specific to Lazy
         [force l] forces the computation on l,
         immediately evaluating it to a value *)
-    let force (l: 'a t): 'a = failwith "Unimplemented" 
+    let force (l: 'a t): 'a = l ()
 end
 
 module NonDeterministic = struct
     type 'a t = 'a list
-    let return a = failwith "Unimplemented"
-    let (>>=) a f = failwith "Unimplemented"
+
+    (** [choose lst] returns a random element from a list [lst] *)
+    let choose lst =
+      let idx = List.length lst |> Int32.of_int |> Random.int32 |> Int32.to_int in
+      List.nth lst idx
+
+    let return a = [a]
+    let (>>=) a f = a |> choose |> f
 end
